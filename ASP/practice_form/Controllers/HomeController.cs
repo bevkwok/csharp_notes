aaced7cc6b3866f26ce80415037f8f0a285ecc9b
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2020-10-13 17:49:05 -0700

    notes about ViewModel

--- ASP/practice_form/Controllers/HomeController.cs
+++ ASP/practice_form/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using practice_form.Models;
 namespace practice_form
 {
     public class HomeController : Controller
@@ -13,9 +15,33 @@ namespace practice_form
         [HttpPost("form")]
         public IActionResult form(string name, string email)
         {
+            //first way of passing info from Controller to View
             ViewBag.Name = name;
             ViewBag.Email = email;
-            return View("form");
+
+            //second way (have to pass someUser in View())
+            // -first need to make a model
+            User someUser = new User()
+            {
+                Name = name,
+                Email = email
+            };
+
+            //Passing a list of User to View
+            User oneUser = new User()
+            {
+                Name = "other",
+                Email = "123@gmail.com"
+            };
+
+            List<User> userList = new List<User>()
+            {
+                someUser, oneUser
+            };
+
+            return View("form", someUser); 
+            //first pass in the string of the View, follow by the object you want to pass in
+            //change someUser to userList if want to pass in the List of User
         }
     }
 }
